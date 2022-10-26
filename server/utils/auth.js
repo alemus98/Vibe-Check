1bea260803bd930a48949e2acfdf4f3231190327
JonWong2020 <jonathannwong50@aol.com>
2022-10-26 11:20:17 -0700

    edited resolvers and typeDefs

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -18,6 +18,7 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
+    
     } catch {
       console.log('Invalid token');
     }
